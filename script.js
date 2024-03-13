9dd907948e59e1a3f4334f7c29fcc9929fcfe279
Xinyue Wang <102383375+mia0204@users.noreply.github.com>
2024-03-13 16:57:50 -0400

    Update script.js
    Use turf to create hexbin map

--- script.js
+++ script.js
@@ -24,7 +24,14 @@ Step 2: VIEW GEOJSON POINT DATA ON MAP
 //HINT: Create an empty variable
 //      Use the fetch method to access the GeoJSON from your online repository
 //      Convert the response to JSON format and then store the response in your new variable
+let collisiongeojson;
 
+fetch('https://raw.githubusercontent.com/mia0204/ggr472-lab4/main/data/pedcyc_collision_06-21.geojson')
+    .then(response => response.json())
+    .then(response => {
+        console.log(response); //Check response in console
+        collisiongeojson = response; // Store geojson as variable using URL from fetch response
+    });
 
 
 /*--------------------------------------------------------------------
@@ -34,6 +41,26 @@ Step 2: VIEW GEOJSON POINT DATA ON MAP
 //      First create a bounding box around the collision point data then store as a feature collection variable
 //      Access and store the bounding box coordinates as an array variable
 //      Use bounding box coordinates as argument in the turf hexgrid function
+map.on('load', () => {
+
+    let bboxgeojson;
+    let bbox = turf.envelope(collisiongeojson); //create bounding box around collision points
+    let bboxscaled = turf.transformScale(bbox, 1.10); //scale bounding box up by 10%
+
+    //put the resulting envelope in a geojson format FeatureCollection
+    bboxgeojson = {
+        "type": "FeatureCollection",
+        "features": [bboxscaled]
+    };
+
+    //create hex grid
+    //must be in order: minX, minY, maxX, maxY. Select from scaled envelope
+    let bboxcoords = [bboxscaled.geometry.coordinates[0][0][0],
+                      bboxscaled.geometry.coordinates[0][0][1],
+                      bboxscaled.geometry.coordinates[0][2][0],
+                      bboxscaled.geometry.coordinates[0][2][1],]
+    let hexgeojson = turf.hexGrid(bboxcoords, 0.5, {units: 'kilometers'});
+
 
 
 
@@ -42,7 +69,17 @@ Step 4: AGGREGATE COLLISIONS BY HEXGRID
 --------------------------------------------------------------------*/
 //HINT: Use Turf collect function to collect all '_id' properties from the collision points data for each heaxagon
 //      View the collect output in the console. Where there are no intersecting points in polygons, arrays will be empty
+let collishex = turf.collect(hexgeojson, collisiongeojson, '_id', 'values');
 
+let maxcollis = 0;
+
+collishex.features.forEach((feature) => {
+    feature.properties.COUNT = feature.properties.values.length
+    if (feature.properties.COUNT > maxcollis) {
+        console.log(feature);
+        maxcollis = feature.properties.COUNT
+    }
+});
 
 
 // /*--------------------------------------------------------------------
@@ -54,5 +91,26 @@ Step 4: AGGREGATE COLLISIONS BY HEXGRID
 //        - The COUNT attribute
 //        - The maximum number of collisions found in a hexagon
 //      Add a legend and additional functionality including pop-up windows
+map.addSource('collis-hex', {
+    type: 'geojson',
+    data: hexgeojson
+});
 
+map.addLayer({
+    'id': 'collis-hex-fill',
+    'type': 'fill',
+    'source': 'collis-hex',
+    'paint': {
+        'fill-color': [
+            'step', //use step expression
+            ['get', 'COUNT'],
+            '#F9E79F',
+            10, '#F39C12',
+            25, '#D35400'
+        ],
+        'fill-opacity': 0.4,
+        'fill-outline-color': "white"
+    }
+});
 
+});
